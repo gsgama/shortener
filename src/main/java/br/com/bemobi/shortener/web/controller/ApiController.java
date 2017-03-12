94ffca801abe76d32889c104c4e50aed4f5abe3e
Guilherme Gama <gsgama@gmail.com>
2017-03-12 16:16:53 -0300

    Criacao de controller para operacoes de baixo nivel na api

---
+++ src/main/java/br/com/bemobi/shortener/web/controller/ApiController.java
@@ -0,0 +1,37 @@
+package br.com.bemobi.shortener.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.bemobi.shortener.domain.entity.ShortUrl;
+import br.com.bemobi.shortener.domain.repository.ShortUrlRepository;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+	@Autowired
+	private ShortUrlRepository shortUrlRepository;
+
+	@GetMapping
+	public List<ShortUrl> list() {
+		return shortUrlRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public ShortUrl get(@PathVariable String id) {
+		return shortUrlRepository.findOne(id);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable String id) {
+		shortUrlRepository.delete(id);
+	}
+
+}
--- src/main/java/br/com/bemobi/shortener/web/controller/ShortUrlController.java
+++ src/main/java/br/com/bemobi/shortener/web/controller/ShortUrlController.java
@@ -1,21 +1,16 @@
 package br.com.bemobi.shortener.web.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import br.com.bemobi.shortener.domain.entity.ShortUrl;
 import br.com.bemobi.shortener.domain.repository.ShortUrlRepository;
@@ -30,35 +25,6 @@ public class ShortUrlController {
 
 	@Autowired
 	private ShortUrlRepository shortUrlRepository;
-	
-	@GetMapping("/all")
-	public List<ShortUrl> list() {
-		return shortUrlRepository.findAll();
-	}
-
-	/*
-	@GetMapping("/{id}")
-	public ShortUrl get(@PathVariable String id) {
-		return shortUrlRepository.findOne(id);
-	}
-	
-	@PostMapping
-	public ResponseEntity<?> save(@RequestBody ShortUrl shortUrl) {
-		shortUrlRepository.save(shortUrl);
-		return ResponseEntity.ok(shortUrl);
-	}
-
-	@PutMapping("/{id}")
-	public ResponseEntity<?> update(@RequestBody ShortUrl shortUrl) {
-		shortUrlRepository.save(shortUrl);
-		return ResponseEntity.ok(shortUrl);
-	}
-
-	@DeleteMapping("/{id}")
-	public void delete(@PathVariable String id) {
-		shortUrlRepository.delete(id);
-	}
-	*/
 	
 	@PutMapping("/create")
 	public ResponseEntity<?> create(@RequestParam(value="url", required=true) String url, @RequestParam(value="CUSTOM_ALIAS", required=false, defaultValue="") String customAlias) {
@@ -99,21 +65,6 @@ public class ShortUrlController {
 	@GetMapping("/notFound/{alias}")
 	public ResponseEntity<?> notFound(@PathVariable String alias) {
 		return ResponseEntity.ok(new ErrorModel("002", "SHORTENED URL NOT FOUND"));
-	}
-	
-	@GetMapping("/test")
-	public ShortUrlModel test() {
-		long startTime = System.currentTimeMillis();
-		try {
-			Thread.sleep(250);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		long stopTime = System.currentTimeMillis();
-		long elapsedTime = stopTime - startTime;
-		ShortUrlModel successOperation = new ShortUrlModel("http://www.google.com", "ggl", String.format("%dms", elapsedTime));
-		return successOperation;
 	}
 }
 
