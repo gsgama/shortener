682b04239963270d85d5ec76d84a8ae1f91d97ca
Guilherme Gama <gsgama@gmail.com>
2017-03-13 06:31:18 -0300

    Removendo imports não utilizados

--- src/main/java/br/com/bemobi/shortener/web/controller/ShortUrlController.java
+++ src/main/java/br/com/bemobi/shortener/web/controller/ShortUrlController.java
@@ -4,11 +4,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
