0bf761f365abeb5c7f4ecd62c8c316152bf6025c
Guilherme Gama <gsgama@gmail.com>
2017-03-13 11:00:38 -0300

    Testes para quando não for possível obter a url

--- src/test/java/br/com/bemobi/shortener/web/ShortUrlControllerTests.java
+++ src/test/java/br/com/bemobi/shortener/web/ShortUrlControllerTests.java
@@ -2,6 +2,7 @@ package br.com.bemobi.shortener.web;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import br.com.bemobi.shortener.domain.entity.ShortUrl;
 import br.com.bemobi.shortener.services.ShortUrlService;
@@ -34,11 +36,34 @@ public class ShortUrlControllerTests {
 		ShortUrl shortUrl = new ShortUrl();
 		shortUrl.setAlias("url-alias");
 		shortUrl.setUrl("http://www.url.com/path");
+		
 		given(this.shortUrlService.retrieveUrl("url-alias"))
 			.willReturn(Optional.of(shortUrl));
+		
 		this.mvc.perform(get("/url-alias"))
 			.andExpect(status().is3xxRedirection())
 			.andExpect(redirectedUrl("http://www.url.com/path"));
+	}
+	
+	@Test
+	public void urlNotFound() throws Exception {
+		given(this.shortUrlService.retrieveUrl("dont-exists"))
+			.willReturn(Optional.empty());
+		
+		this.mvc.perform(get("/dont-exists"))
+			.andExpect(forwardedUrl("/notFound/dont-exists"))
+			.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void urlNotFoundMessage() throws Exception {
+		given(this.shortUrlService.retrieveUrl("dont-exists"))
+			.willReturn(Optional.empty());
+		
+		this.mvc.perform(get("/notFound/dont-exists"))
+			.andExpect(status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.err_code").value("002"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.description").value("SHORTENED URL NOT FOUND"));
 	}
 
 }
